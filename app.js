61092ac69f50dcd395e3eaaca42bb86d6386eedd
Charles Burch <georgia_beast89@yahoo.com>
2019-05-11 20:09:11 -0500

    added the product to the html dynamically and cleaned up test product data from html

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cartOverlay = document.querySelector(".cart-overlay");
 const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-center");
+const productsDOM = document.querySelector(".products-center");//inject products into html
 
 //cart
 let cart = [];
@@ -35,7 +35,31 @@ class Products {
 
   //display products
   class UI {
-
+    displayProducts(products) {
+      let result = "";
+      products.forEach(product => {
+        result += `
+     <!-- single product -->
+          <article class="product">
+            <div class="img-container">
+              <img
+                src=${product.image}
+                alt="product"
+                class="product-img"
+              />
+              <button class="bag-btn" data-id=${product.id}>
+                <i class="fas fa-shopping-cart"></i>
+                add to bag
+              </button>
+            </div>
+            <h3>${product.title}</h3>
+            <h4>$${product.price}</h4>
+          </article>
+          <!-- end of single product -->
+     `;
+      });
+      productsDOM.innerHTML = result;
+    }
   }
 
   //local storage
@@ -48,9 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const products = new Products();
   
     // get all products
-    products
-      .getProducts()
-      .then(data => {
-          console.log(data);
-      });
+  products.getProducts().then(products => {
+    ui.displayProducts(products);
+  });
   });
